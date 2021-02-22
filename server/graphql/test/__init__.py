1fae57ad8e2035a8c35bd5da386cbdf1fdc12d4b
liangliang <liangliang@liangdeiMac.local>
2021-02-22 08:38:45 +0800

    同步

--- server/graphql/test/__init__.py
+++ server/graphql/test/__init__.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene import ObjectType, String, Schema
 
+
 class Query(ObjectType):
     # this defines a Field `hello` in our Schema with a single Argument `name`
     hello = String(name=String(default_value="stranger"))
@@ -9,10 +10,15 @@ class Query(ObjectType):
     # our Resolver method takes the GraphQL context (root, info) as well as
     # Argument (name) for the Field and returns data for the query Response
     def resolve_hello(root, info, name):
+        print(root)
+        print(info)
         return f'Hello {name}!'
 
     def resolve_goodbye(root, info):
         return 'See ya!'
 
 schema = Schema(query=Query)
+# query_string = '{ hello }'
+# result = schema.execute(query_string)
+# print(result.data['hello'])
 
