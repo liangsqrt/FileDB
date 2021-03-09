65fdada608e1bbc61b8d16281aae9a24648e3a4c
liangliang <liangliang@liangdeiMac.local>
2021-03-10 02:02:56 +0800

    完成了查询,格式化的两个功能

--- filedb/db/formater.py
+++ filedb/db/formater.py
@@ -1,38 +1,16 @@
 class Formater(object):
-    format = {}
-    def __init__(self, format_dict: dict):
-        assert isinstance(format_dict, dict), "format_dict must be dict like"
-        self.format = format_dict
-    
-    def generate_result(self, input_data):
-        result  = {}
-        if 1 not in self.format.values():
-            for _k,_v in self.format.items():
-                if _v == 0:
-                    continue
-                result[_k] = input_data.get(_k)
-        else:
-            pass
-    
     @staticmethod
-    def generate_dict_path(format_dict):
-        for _k, _v in format_dict.items():
-            if(format_dict[_k]):
-                pass
-
-
-def formater(format_dict, data_dict):
-    for _k, _ in list(data_dict.items()):
-        _f_v = format_dict.get(_k)
-        if _f_v == 1:
-            continue
-        elif _f_v:
-            target = formater(format_dict.get(_k), data_dict.get(_k))
-            data_dict[_k] = target
-        else:
-            del data_dict[_k]
-            
-    return data_dict
+    def format(format_dict, data_dict):
+        for _k, _ in list(data_dict.items()):
+            _f_v = format_dict.get(_k)
+            if _f_v == 1:
+                continue
+            elif _f_v:
+                target = Formater.format(format_dict.get(_k), data_dict.get(_k))
+                data_dict[_k] = target
+            else:
+                del data_dict[_k]
+        return data_dict
 
 
 
@@ -53,12 +31,13 @@ if __name__ == '__main__':
     }
     
     format_a = {
-        "a":1,
+        "a":{"b":1, "c":0},
         "b": {
-            "e":1
+            "e":1,
+            "f":1
         }
     }
-    print(formater(format_a, a))
+    print(Formater.format(format_a, a))
     
 
             
